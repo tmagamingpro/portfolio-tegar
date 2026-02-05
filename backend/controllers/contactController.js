6d50f85185e77093805cea1280e38368885d0a4d
Tegar <tegarafrian2006@gmail.com>
2026-02-05 19:38:06 +0700

    done

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -2,7 +2,6 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { randomUUID } from 'crypto';
-import { random } from 'animejs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
